e80ae07352d19c5d9de997d8c897c480cb3e5536
jdavidintermixit <jdavid@intermixit.com>
2026-03-06 09:58:35 -0500

    Update fabric-client.js

--- src/fabric-client.js
+++ src/fabric-client.js
@@ -47,6 +47,10 @@ class FabricClient {
         trustServerCertificate: false,
         connectTimeout: 30000,
         requestTimeout: 60000,
+        cryptoCredentialsDetails: {
+          minVersion: 'TLSv1.2',
+          ciphers: 'DEFAULT:@SECLEVEL=0',
+        },
       },
       pool: {
         max: 10,
